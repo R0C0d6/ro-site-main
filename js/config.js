812e45ac9ab0ae094d57107c98ecc094249c5d99
R0C0d6 <awukurolandmawuli@gmail.com>
2025-10-17 17:13:32 +0000

    new changes

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_uA8yRdy96', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1jfinlcuis42snquhle1p8grts', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
